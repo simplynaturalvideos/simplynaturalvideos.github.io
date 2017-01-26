c7670a7da53ac2356b261d1afa9f843107c727a1
simplynaturalvideos <simplynaturalvideos@gmail.com>
2017-01-25 17:18:00 -0700

    Initial site

---
+++ wp-content/themes/brehoh-one-page-portfolio-theme/js/loader.js
@@ -0,0 +1,8 @@
+(function ($) {
+    "use strict";
+    //preloader
+    $(window).on("load", function () { // makes sure the whole site is loaded
+        $("#status").fadeOut(); // will first fade out the loading animation
+        $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
+    });	
+})(jQuery);

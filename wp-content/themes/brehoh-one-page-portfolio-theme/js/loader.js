6e936269ae69f054df0960ac74435450729a8b6e
simplynaturalvideos <simplynaturalvideos@gmail.com>
2017-01-25 17:18:54 -0700

    Revert "Initial site"
    This reverts commit c7670a7da53ac2356b261d1afa9f843107c727a1.

--- wp-content/themes/brehoh-one-page-portfolio-theme/js/loader.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    "use strict";
-    //preloader
-    $(window).on("load", function () { // makes sure the whole site is loaded
-        $("#status").fadeOut(); // will first fade out the loading animation
-        $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
-    });	
-})(jQuery);
